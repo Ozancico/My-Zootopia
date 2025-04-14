a6e529f5b30d16f32e051d0fe5c1ef1af9e7d550
Ozancico <ozancicek.oc@gmail.com>
2025-04-15 01:36:49 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,28 +5,33 @@ def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
-animals_data = load_data('animals_data.json')
+def generate_animal_output(data):
+    """Generates formatted string of animal info"""
+    output = ''
+    for animal in data:
+        if 'name' in animal:
+            output += f"Name: {animal['name']}\n"
+        if 'characteristics' in animal and 'diet' in animal['characteristics']:
+            output += f"Diet: {animal['characteristics']['diet']}\n"
+        if 'locations' in animal and animal['locations']:
+            output += f"Location: {animal['locations'][0]}\n"
+        if 'type' in animal:
+            output += f"Type: {animal['type']}\n"
+        output += '\n'
+    return output
 
-for animal in animals_data:
+def replace_template(html_template_path, output_text, result_path):
+    with open(html_template_path, "r") as template_file:
+        template_content = template_file.read()
 
-    if "name" in animal:
-        print(f"Name: {animal['name']}")
+    final_html = template_content.replace("__REPLACE_ANIMALS_INFO__", output_text)
 
-    if "characteristics" in animal and "diet" in animal["characteristics"]:
-        print(f"Diet: {animal['characteristics']['diet']}")
+    with open(result_path, "w") as result_file:
+        result_file.write(final_html)
 
-    if "locations" in animal and animal["locations"]:
-        print(f"Location: {animal['locations'][0]}")
-
-    if "characteristics" in animal and "type" in animal["characteristics"]:
-        print(f"Type: {animal['characteristics']['type']}")
-
-    print("...")
+    print(f"HTML successfully written to {result_path}")
 
 
-
-
-
-
-
-
+data = load_data('animals_data.json')
+animal_info = generate_animal_output(data)
+replace_template('animals_template.html', animal_info, 'animals.html')
